ce0fddba5c7b04cc18a2eea1fb01de5c97b0c8a2
kazuuud <81429095+k-jinma@users.noreply.github.com>
2021-08-16 11:08:12 +0900

    Update 顧客表.sql
    表名を修正

--- DBテキスト演習/顧客表.sql
+++ DBテキスト演習/顧客表.sql
@@ -1,11 +1,11 @@
 use orders;
 
-CREATE Table customer (
+CREATE Table customers (
     no VARCHAR(4) NOT NULL,
     name VARCHAR(30) NOT NULL,
     address VARCHAR(100),
     PRIMARY KEY(no)
 );
 
-insert into customer (no, name, address)
+insert into customers (no, name, address)
     values ('U001', '山田一郎', '東京都');
